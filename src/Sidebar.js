8a4c406aec69c34cbab4137238b7f812233b0709
Rohit Gaur <rohitgaur7@gmail.com>
2021-01-29 21:47:16 +0530

    style: sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar__container">Sidebar</div>;
+  return (
+    <div className="sidebar__container">
+      <div className="sidebar__logoContainer">
+        <img className="sidebar__logo" src="img/rohit-logo.gif" alt="" />
+      </div>
+      <div className="sidebar__profilePicContainer fadeIn">
+        <img className="sidebar__profilePic" src="img/dp.jpg" alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
