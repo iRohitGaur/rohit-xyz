9f60d8742c55bd4c6e4dfd11d3f130cc259f3aaf
Rohit Gaur <rohitgaur7@gmail.com>
2021-01-31 12:31:52 +0530

    add fontAwesome

--- src/pages/home/Sidebar.js
+++ src/pages/home/Sidebar.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Sidebar.css";
 import ReactTypingEffect from "react-typing-effect";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fab);
 
 function Sidebar() {
   return (
@@ -16,7 +21,7 @@ function Sidebar() {
           className="sidebar__introText"
           text={"Hi, I'm Rohit Gaur"}
           speed="100"
-          typingDelay="900"
+          typingDelay="0"
           cursor=" "
           eraseDelay="99999999999999"
           displayTextRenderer={(text, i) => {
@@ -28,7 +33,7 @@ function Sidebar() {
           className="sidebar__introText"
           text={"iOS and Web developer"}
           speed="100"
-          typingDelay="3000"
+          typingDelay="2000"
           cursor=" "
           eraseDelay="99999999999999"
         />
@@ -38,9 +43,24 @@ function Sidebar() {
         <div>Blogs</div>
       </div>
       <div className="sidebar__social">
-        <span>Twitter</span>
-        <span>Github</span>
-        <span>Instagram</span>
+        <a href="https://apps.apple.com/us/developer/rohit-gaur/id1382308402">
+          <FontAwesomeIcon icon={["fab", "apple"]} />
+        </a>
+        <a href="https://github.com/iRohitGaur">
+          <FontAwesomeIcon icon={["fab", "github"]} />
+        </a>
+        <a href="https://twitter.com/iRohitGaur">
+          <FontAwesomeIcon icon={["fab", "twitter"]} />
+        </a>
+        <a href="https://www.linkedin.com/in/iRohitGaur">
+          <FontAwesomeIcon icon={["fab", "linkedin"]} />
+        </a>
+        <a href="https://www.youtube.com/alrodev">
+          <FontAwesomeIcon icon={["fab", "youtube"]} />
+        </a>
+        <a href="https://www.instagram.com/iRohitGaur">
+          <FontAwesomeIcon icon={["fab", "instagram"]} />
+        </a>
       </div>
     </div>
   );
