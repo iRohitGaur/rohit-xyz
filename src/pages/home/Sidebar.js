3a14dd5b7861a6754da66923e688dbf65ddbcc1d
Rohit Gaur <rohitgaur7@gmail.com>
2021-02-05 09:23:38 +0530

    style: responsive

--- src/pages/home/Sidebar.js
+++ src/pages/home/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 import ReactTypingEffect from "react-typing-effect";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,60 +8,90 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fab);
 
 function Sidebar({ content, contentHandler }) {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [visible, setVisible] = useState(false);
+
+  const updateWidth = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  }, []);
+
   return (
     <div className="sidebar__container">
       <div className="sidebar__logoContainer">
         <img className="sidebar__logo" src="img/rohit-logo.gif" alt="" />
       </div>
-      <div className="sidebar__profilePicContainer fadeIn">
-        <img className="sidebar__profilePic" src="img/dp.jpg" alt="" />
-        <h1>Rohit Gaur</h1>
-        <ReactTypingEffect
-          className="sidebar__introText"
-          text={"iOS and Web developer"}
-          speed="100"
-          typingDelay="0"
-          cursor=" "
-          eraseDelay="99999999999999"
-        />
-      </div>
-      <div className="sidebar__nav">
+      {width < 1041 && (
         <div
-          className={`sidebar__navButton ${
-            content === "portfolio" ? "selected" : ""
-          }
-          `}
-          onClick={() => contentHandler("portfolio")}
+          className="sidebar__burgerButton"
+          onClick={() => {
+            visible ? setVisible(false) : setVisible(true);
+          }}
         >
-          Portfolio
+          {visible ? "X" : "☰"}
         </div>
-        <div
-          className={`sidebar__navButton ${content === "blog" ? "selected" : ""}
+      )}
+      <div
+        className={`sidebar__burgerContent ${
+          width < 1041 ? (visible ? "visible" : "invisible") : ""
+        }`}
+      >
+        <div className="sidebar__profilePicContainer fadeIn">
+          <img className="sidebar__profilePic" src="img/dp.jpg" alt="" />
+          <h1 className="sidebar__heroTitle">Rohit Gaur</h1>
+          <ReactTypingEffect
+            className="sidebar__introText"
+            text={"iOS and Web developer"}
+            speed="100"
+            typingDelay="0"
+            cursor=" "
+            eraseDelay="99999999999999"
+          />
+        </div>
+        <div className="sidebar__nav">
+          <div
+            className={`sidebar__navButton ${
+              content === "portfolio" ? "selected" : ""
+            }
           `}
-          onClick={() => contentHandler("blog")}
-        >
-          Blog
+            onClick={() => contentHandler("portfolio")}
+          >
+            Portfolio
+          </div>
+          <div
+            className={`sidebar__navButton ${
+              content === "blog" ? "selected" : ""
+            }
+          `}
+            onClick={() => contentHandler("blog")}
+          >
+            Blog
+          </div>
         </div>
-      </div>
-      <div className="sidebar__social">
-        <a href="https://apps.apple.com/us/developer/rohit-gaur/id1382308402">
-          <FontAwesomeIcon icon={["fab", "apple"]} />
-        </a>
-        <a href="https://github.com/iRohitGaur">
-          <FontAwesomeIcon icon={["fab", "github"]} />
-        </a>
-        <a href="https://twitter.com/iRohitGaur">
-          <FontAwesomeIcon icon={["fab", "twitter"]} />
-        </a>
-        <a href="https://www.linkedin.com/in/iRohitGaur">
-          <FontAwesomeIcon icon={["fab", "linkedin"]} />
-        </a>
-        <a href="https://www.youtube.com/alrodev">
-          <FontAwesomeIcon icon={["fab", "youtube"]} />
-        </a>
-        <a href="https://www.instagram.com/iRohitGaur">
-          <FontAwesomeIcon icon={["fab", "instagram"]} />
-        </a>
+        <div className="sidebar__social">
+          <a href="https://apps.apple.com/us/developer/rohit-gaur/id1382308402">
+            <FontAwesomeIcon icon={["fab", "apple"]} />
+          </a>
+          <a href="https://github.com/iRohitGaur">
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </a>
+          <a href="https://twitter.com/iRohitGaur">
+            <FontAwesomeIcon icon={["fab", "twitter"]} />
+          </a>
+          <a href="https://www.linkedin.com/in/iRohitGaur">
+            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          </a>
+          <a href="https://www.youtube.com/alrodev">
+            <FontAwesomeIcon icon={["fab", "youtube"]} />
+          </a>
+          <a href="https://www.instagram.com/iRohitGaur">
+            <FontAwesomeIcon icon={["fab", "instagram"]} />
+          </a>
+        </div>
       </div>
     </div>
   );
